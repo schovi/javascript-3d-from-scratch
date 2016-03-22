a82d35855fae722e21b5c6a3e8d64b70d7f211eb
David Schovanec <schovanec@schovi.cz>
2016-03-22 22:06:22 +0100

    Add variations of pixel rendering and use fastest one

--- src/render/canvas.js
+++ src/render/canvas.js
@@ -15,23 +15,45 @@ export default class Canvas {
     this.setup && this.setup()
   }
 
-  // Render pixel with usage 1x1 putImageData
-  // Pros: supports subpixel (kind of antialiasing)
-  // Cons: is slow
-  setup() {
-    this.imageData = this.context.createImageData(1,1)
-    this.buffer    = this.imageData.data
+  renderLine(x0, y0, x1, y1, color) {
+    renderLine(this, x0, y0, x1, y1, color)
   }
 
+  //////////////////////////
+  // Render pixel variants
+
+  // // Render pixel with usage 1x1 putImageData
+  // // Pros: supports subpixel (kind of antialiasing)
+  // // Cons: is slow
+  // setup() {
+  //   this.imageData = this.context.createImageData(1,1)
+  //   this.buffer    = this.imageData.data
+  // }
+  //
+  // pixel(x, y, color) {
+  //   const [r, g, b, a] = color
+  //
+  //   this.buffer[0] = r
+  //   this.buffer[1] = g
+  //   this.buffer[2] = b
+  //   this.buffer[3] = a
+  //
+  //   this.context.putImageData(this.imageData, x, y)
+  // }
+  //
+  // beforeRender() {
+  //   // Clear canvas before each render
+  //   this.context.clearRect(0, 0, this.width, this.height)
+  // }
+
+  // Render pixel with usage fillRect
+  // Pros: faster then putImageData
+  // Cons: 6x faster than 1x1 putImageData
   pixel(x, y, color) {
     const [r, g, b, a] = color
 
-    this.buffer[0] = r
-    this.buffer[1] = g
-    this.buffer[2] = b
-    this.buffer[3] = a
-
-    this.context.putImageData(this.imageData, x, y)
+    this.context.fillStyle = "rgba("+r+","+g+","+b+","+(a/255)+")";
+    this.context.fillRect( x, y, 1, 1 );
   }
 
   beforeRender() {
@@ -39,11 +61,33 @@ export default class Canvas {
     this.context.clearRect(0, 0, this.width, this.height)
   }
 
-  afterRender() {
-
-  }
-
-  renderLine(x0, y0, x1, y1, color) {
-    renderLine(this, x0, y0, x1, y1, color)
-  }
+  // // Render pixel with usage one putImageData
+  // // Pros:
+  // // Cons: 2x slower than fillRect
+  // pixel(x, y, [r, g, b, a]) {
+  //   x = Math.round(x)
+  //   y = Math.round(y)
+  //
+  //   // Check for pixels outside of viewport
+  //   if(x < 0 || x >= this.width || y < 0 || y > this.height) {
+  //     return
+  //   }
+  //
+  //   const index = (this.width * y + x) * 4
+  //
+  //   // Fill color
+  //   this.buffer[index + 0] = r;
+  //   this.buffer[index + 1] = g;
+  //   this.buffer[index + 2] = b;
+  //   this.buffer[index + 3] = a;
+  //     }
+  //
+  // beforeRender() {
+  //   this.imageData = this.context.createImageData(this.width, this.height)
+  //   this.buffer    = this.imageData.data
+  // }
+  //
+  // afterRender() {
+  //   this.context.putImageData(this.imageData, 0, 0)
+  // }
 }
